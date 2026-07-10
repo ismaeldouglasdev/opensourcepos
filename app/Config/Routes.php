cfdad3b33a8ab746d0022da453fdcaacab19666f
Ismael Douglas <ismaeldouglasdev@users.noreply.github.com>
2026-07-10 10:26:44 -0300

    fix: remover setAutoRoute(false) - OSPOS depende de auto-routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->setAutoRoute(false);
 $routes->setDefaultController('Login');
 
 $routes->get('/', 'Login::index');
